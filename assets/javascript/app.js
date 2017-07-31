5e576c86c9a058f9a6f37023ec48a0dc3ad35134
dcal588 <calderon.david@gmail.com>
2017-07-31 02:25:05 -0700

    Update app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,6 @@
 $ (document).ready( function(){	
 	var questions = ["The Fantastic Four have the headquarters in what building?", "Thor has two war goats to pull his chariot. They are named:", "S.H.I.E.L.D.'s highest ranking agent is:", "Captain America was frozen in which war?", "Deadpool joined the Weapon X program because:"];
 	var answers = ["Stark Tower", "Fantastic Headquarters", "Xavier Institute", "Baxter Building", "Thunder and Lightning", "Balder and Hermod", "Ask and Embla", "Toothgrinder and Toothgnasher", "Steven Rogers", "Peter Parker", "Natalia Romanova", "Nick Fury", "World War I", "Cold War", "American Civil War", "World War II", "He was forced to", "He thought it would be fun", "He wanted to fight for justice", "He had incurable cancer",];
-	//var choicesMemory = [];
 	var timeCounter = 30;
 	var intervalId;
 	var questionNumber = 0;
@@ -12,84 +11,81 @@ $ (document).ready( function(){
 
 	function ender() {
 		if(timeCounter===0) {
-    		$("#messageBox").css("display", "block");
-				$("#message").text("Game Over!");
-				$("#correctAnswer").text("");
-				$("#rightAnswerCount").text("Number Right: " + numberRight);
-				$("#wrongAnswerCount").text("Number Wrong: " + numberWrong);
-				$("#restart").css("display", "block");
+    			$("#messageBox").css("display", "block");
+			$("#message").text("Game Over!");
+			$("#correctAnswer").text("");
+			$("#rightAnswerCount").text("Number Right: " + numberRight);
+			$("#wrongAnswerCount").text("Number Wrong: " + numberWrong);
+			$("#restart").css("display", "block");
 		}
 	}
 
-		function restart() {
-			questionNumber=0;
-			numberRight=0;
-			numberWrong=0;
-			next();
-			placer();
-			$("#messageBox").css("display", "none");
-			$("#restart").css("display", "none");
-    	responsePlaced = [];
-
+	function restart() {
+		questionNumber=0;
+		numberRight=0;
+		numberWrong=0;
+		next();
+		placer();
+		$("#messageBox").css("display", "none");
+		$("#restart").css("display", "none");
+    		responsePlaced = [];
 	}
+	
 	$("#restart").on("click", restart);
 
 	$('#options input').on('change', function(i) {
-			if (questionNumber<questions.length) {
-   			var choice = i.currentTarget.nextSibling.innerHTML;
-	  	//choicesMemory.push(choice);
-				if (choice === answers[3] || choice === answers[7] || choice === answers[11] ||choice === answers[15] ||choice === answers[19]){
-					numberRight++
-					$(this).prop('checked', false);
-    			console.log("right:" + numberRight);
-    			next();
-    			placer();
-      		if ($('#messageBox').css('display')==="block") {
-						$("#messageBox").css("display", "none");
-    			}
-    			else {
-    				$("#messageBox").css("display", "block");
-						$("#message").text("Correct!");
-						$("#correctAnswer").text("");
-						$("#rightAnswerCount").text("Number Right: " + numberRight);
-						$("#wrongAnswerCount").text("Number Wrong: " + numberWrong);
-						questionNumber++
-    			}
-				}
-				else {
-					numberWrong++
-					$(this).prop('checked', false);
-    			console.log("wrong:"+numberWrong);
-    			next();
-      		placer();
-      		if ($('#messageBox').css('display')==="block") {
-						$("#messageBox").css("display", "none");
-    			}
-    			else {
-    				$("#messageBox").css("display", "block");
-						$("#message").text("Wrong!");
-						$("#correctAnswer").text("Correct Answer was:" + answers[(questionNumber/.25)+3]);
-						$("#rightAnswerCount").text("Number Right: " + numberRight);
-						$("#wrongAnswerCount").text("Number Wrong: " + numberWrong);
-						questionNumber++
-    			}
-				}
+		if (questionNumber<questions.length) {
+			var choice = i.currentTarget.nextSibling.innerHTML;
+			if (choice === answers[3] || choice === answers[7] || choice === answers[11] ||choice === answers[15] ||choice === answers[19]){
+				numberRight++
+				$(this).prop('checked', false);
+    				next();
+    				placer();
+      				if ($('#messageBox').css('display')==="block") {
+					$("#messageBox").css("display", "none");
+    				}
+    				else {
+    					$("#messageBox").css("display", "block");
+					$("#message").text("Correct!");
+					$("#correctAnswer").text("");
+					$("#rightAnswerCount").text("Number Right: " + numberRight);
+					$("#wrongAnswerCount").text("Number Wrong: " + numberWrong);
+					questionNumber++
+    				}
 			}
 			else {
+				numberWrong++
+				$(this).prop('checked', false);
+    				next();
+      				placer();
+      				if ($('#messageBox').css('display')==="block") {
+					$("#messageBox").css("display", "none");
+    				}
+    				else {
+    					$("#messageBox").css("display", "block");
+					$("#message").text("Wrong!");
+					$("#correctAnswer").text("Correct Answer was:" + answers[(questionNumber/.25)+3]);
+					$("#rightAnswerCount").text("Number Right: " + numberRight);
+					$("#wrongAnswerCount").text("Number Wrong: " + numberWrong);
+					questionNumber++
+    				}
+			}
+		}
+		else {
 			stop();
 			ender();
-			}
+		}
 	});
 
 	function run() {
 		intervalId = setInterval(countDown, 1000);
 	}
-  function stop() {
-  	if(timeCounter===0) {
-    	clearInterval(intervalId);
-  	}
-  }
-
+  
+	function stop() {
+  		if(timeCounter===0) {
+    			clearInterval(intervalId);
+  		}
+	}
 	function countDown() {
 		timeCounter--
 		$("#timer").html(timeCounter);
@@ -99,16 +95,16 @@ $ (document).ready( function(){
 				placer();
 				if ($('#messageBox').css('display')==="block") {
 					$("#messageBox").css("display", "none");
-    		}
-    		else {
-    			questionNumber++
+    				}
+    				else {
+    					questionNumber++
 					numberWrong++
-    			$("#messageBox").css("display", "block");
+    					$("#messageBox").css("display", "block");
 					$("#message").text("Time Up!");
 					$("#correctAnswer").text("Correct Answer was:" + answers[(questionNumber/.25)+3]);
-						$("#rightAnswerCount").text("Number Right: " + numberRight);
-						$("#wrongAnswerCount").text("Number Wrong: " + numberWrong);
-    		}
+					$("#rightAnswerCount").text("Number Right: " + numberRight);
+					$("#wrongAnswerCount").text("Number Wrong: " + numberWrong);
+    				}
 			}
 		}
 		else {
@@ -117,16 +113,16 @@ $ (document).ready( function(){
 		}
 	}
 	function next() {
-			clearInterval(intervalId);
-    	responsePlaced = [];
-    	if ($('#messageBox').css('display')==="block") {
-    		timeCounter = 30;
-    	}
-    	else {
-    		timeCounter = 5;
-    	}
-			$("#timer").html(timeCounter);
-			run();
+		clearInterval(intervalId);
+    		responsePlaced = [];
+    		if ($('#messageBox').css('display')==="block") {
+    			timeCounter = 30;
+    		}
+    		else {
+    			timeCounter = 5;
+    		}
+		$("#timer").html(timeCounter);
+		run();
 	}
 	
 	function placer() {
@@ -136,11 +132,12 @@ $ (document).ready( function(){
 				var randomPosition = Math.floor(Math.random() * position.length);
 			}
 			while (isPositionAssigned());
-		 		$("#choice"+ randomPosition).html(answers[(questionNumber*4)+i]);
-				responsePlaced.push(randomPosition); 
-	  		}
-			function isPositionAssigned () {
-				for (i = 0; i < responsePlaced.length; i++) {
+		 	$("#choice"+ randomPosition).html(answers[(questionNumber*4)+i]);
+			responsePlaced.push(randomPosition); 
+	  	}
+		
+		function isPositionAssigned () {
+			for (i = 0; i < responsePlaced.length; i++) {
 				if (randomPosition === responsePlaced[i]) {
 					return true;
 				}
@@ -148,6 +145,6 @@ $ (document).ready( function(){
 			return false;
 		}
   	}
-  	placer();
-		run();
+	placer();
+	run();
 });
